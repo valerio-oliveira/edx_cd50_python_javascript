8ac3ae7ed2c4bb702969bc8d59015a7ce7564901
valerio-oliveira <valerio.net@gmail.com>
2021-06-08 21:41:23 -0300

    Add 03_python directory

---
+++ 03_python/conditions.py
@@ -0,0 +1,10 @@
+n = int(input("Number: "))
+
+if n > 0:
+    print(f"{n} is positive")
+elif n == 0:
+    print(f"{n} is zero")
+else:
+    print(f"{n} is negative")
+
+
---
+++ 03_python/sequences.py
@@ -0,0 +1,7 @@
+name = "Harry"
+
+print(name[2])
+
+names = ["Harry", "Ron", "Hermione"]
+
+print(names[2])
