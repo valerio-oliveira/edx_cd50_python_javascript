e806b62f13511ae497359a1c69afbb04812fa15a
Valerio Oliveira <valerio.oliveira@grupomateus.com.br>
2021-06-11 08:43:22 -0300

    Change and hide secret key 4th try

--- 05_sql_models_migrations/airline/secret_settings.py
+++
@@ -1,3 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5w#x@zwf8jy2@hpp3titz0_vy8y@vnwj=n^q8y84cd+fojq^m0'
-# SECRET_KEY = 'django-insecure-5w8jy2@hpn^q8y84cd+p3titz0_vy8y@#x@zwfvnwj=fojq^m0'
